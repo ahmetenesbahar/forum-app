5e882e92c75832643a8b7370a89dd035837cb297
ahmetenesbahar <enesbahar2303@gmail.com>
2023-12-08 14:57:17 +0300

    feat: login register

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -30,8 +30,31 @@ export const register = async (req, res) => {
       interestedCategories,
     });
 
-    const savedUzer = await newUser.save();
+    const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+//LOGGING IN
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;
+    res.status(200).json({ token, user });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
