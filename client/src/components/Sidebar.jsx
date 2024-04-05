4a837cbe439146e9aaff3f43760aea0990b71ae3
ahmetenesbahar <enesbahar2303@gmail.com>
2024-04-05 16:03:59 +0300

    feat: transition

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { useTheme } from "./contexts/ThemeContext";
+import { Transition } from "@headlessui/react";
+import { RiArrowUpSLine, RiArrowDownSLine } from "react-icons/ri";
 
 const Sidebar = () => {
   const { theme } = useTheme();
+  const [isShowing, setIsShowing] = useState(true);
   const [communities, setCommunities] = useState([]);
   useEffect(() => {
     try {
@@ -17,28 +20,55 @@ const Sidebar = () => {
     }
   }, []);
   return (
-    <div className={`${theme.borderGray} border-r-2 w-1/6`}>
-      <h1 className={`${theme.text} text-lg font-bold p-4 `}>Communities</h1>
-      <ul>
-        {communities.map((community) => (
-          <li
-            key={community._id}
-            className={`${theme.text} p-3 border-b ${theme.borderGray} ${theme.hoverBackground} hover:cursor-pointer`}
-          >
-            <div className="flex gap-2 items-center ">
-              <img
-                src={`http://localhost:3001/assets/${community?.picturePath}`}
-                alt=""
-                className="w-8 h-8 rounded-full object-cover"
-              />
-              <h3 className="font-medium">
-                <span className="font-normal">f/</span>
-                {community.communityName}
-              </h3>
-            </div>
-          </li>
-        ))}
-      </ul>
+    <div className={`${theme.borderGray} border-r-2 w-80`}>
+      <div className="communities">
+        <div
+          className={`flex items-center cursor-pointer ${theme.hoverBackground}`}
+          onClick={() => setIsShowing((isShowing) => !isShowing)}
+        >
+          <h1 className={`${theme.text} text-lg font-bold p-4 select-none `}>
+            Communities
+          </h1>
+
+          <RiArrowUpSLine
+            className={
+              isShowing
+                ? "w-6 h-6 rotate-0 ease-linear duration-75 "
+                : "w-6 h-6 rotate-180 ease-linear duration-75"
+            }
+          />
+        </div>
+        <Transition
+          show={isShowing}
+          enter="transition-opacity duration-75"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="transition-opacity duration-150"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <ul>
+            {communities.map((community) => (
+              <li
+                key={community._id}
+                className={`${theme.text} p-3 border-b ${theme.borderGray} ${theme.hoverBackground} hover:cursor-pointer select-none`}
+              >
+                <div className="flex gap-2 items-center ">
+                  <img
+                    src={`http://localhost:3001/assets/${community?.picturePath}`}
+                    alt=""
+                    className="w-8 h-8 rounded-full object-cover"
+                  />
+                  <h3 className="font-medium ">
+                    <span className="font-normal ">f/</span>
+                    {community.communityName}
+                  </h3>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </Transition>
+      </div>
     </div>
   );
 };
