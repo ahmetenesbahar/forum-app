77e7a647562458b4cb16106e79a7153964b72383
ahmetenesbahar <enesbahar2303@gmail.com>
2024-04-05 13:42:44 +0300

    feat: listing communities on sidebar

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,9 +1,50 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTheme } from "./contexts/ThemeContext";
 
 const Sidebar = () => {
   const { theme } = useTheme();
-  return <div className={`${theme.borderGray} border-r-2 w-1/6`}></div>;
+  const [communities, setCommunities] = useState([]);
+  useEffect(() => {
+    try {
+      const fetchCommunities = async () => {
+        const response = await fetch("http://localhost:3001/communities");
+        const data = await response.json();
+        setCommunities(data);
+      };
+      fetchCommunities();
+    } catch (error) {
+      console.log(error);
+    }
+  }, []);
+  return (
+    <div className={`${theme.borderGray} border-r-2 w-1/6`}>
+      <h1
+        className={`${theme.text} text-lg font-bold p-4 border-b ${theme.borderGray}`}
+      >
+        Communities
+      </h1>
+      <ul>
+        {communities.map((community) => (
+          <li
+            key={community._id}
+            className={`${theme.text} p-3 border-b ${theme.borderGray} ${theme.hoverBackground} hover:cursor-pointer`}
+          >
+            <div className="flex gap-2 items-center ">
+              <img
+                src={`http://localhost:3001/assets/${community?.picturePath}`}
+                alt=""
+                className="w-8 h-8 rounded-full object-cover"
+              />
+              <h3 className="font-medium">
+                <span className="font-normal">f/</span>
+                {community.communityName}
+              </h3>
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default Sidebar;
