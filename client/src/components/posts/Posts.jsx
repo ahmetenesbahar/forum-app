397a9e32a6b0aada68967eec5322daba5292edd7
ahmetenesbahar <enesbahar2303@gmail.com>
2024-04-29 23:33:14 +0300

    feat: posts fetched and displayed perfectly

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchPosts } from "state/postSlice";
 
 const Posts = () => {
-  return <div>Posts</div>;
+  const dispatch = useDispatch();
+  const { posts, status, error } = useSelector((state) => state.posts);
+  const token = useSelector((state) => state.auth.token);
+
+  useEffect(() => {
+    dispatch(fetchPosts(token));
+  }, [dispatch, token]);
+
+  return (
+    <div>
+      {posts.map((post) => (
+        <div key={post._id}>
+          {console.log(post._id, post.title, post.picturePath)}
+          <p className="text-white">{post.title}</p>
+          <img
+            src={`http://localhost:3001/assets/${post?.picturePath}`}
+            alt=""
+            className="w-10 h-10"
+          />
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
