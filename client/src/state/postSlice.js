f3fe4628d950a55ef3579c02506ed367b36d0000
ahmetenesbahar <enesbahar2303@gmail.com>
2024-04-29 22:33:54 +0300

    feat: fetch post reducer

--- client/src/state/postSlice.js
+++ client/src/state/postSlice.js
@@ -1,7 +1,47 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
-export const postSlice = createSlice({
-  name: "post",
+export const fetchPosts = createAsyncThunk("post/getPosts", async () => {
+  try {
+    const response = await fetch("http://localhost:3001/posts/getPosts", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
+    });
+    if (!response.ok) {
+      throw new Error("Postlar alınamadı.");
+    }
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
 });
+
+export const postSlice = createSlice({
+  name: "posts",
+  initialState: {
+    posts: [],
+    status: "idle",
+    error: null,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchPosts.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchPosts.fulfilled, (state, action) => {
+        state.status = "succeeded";
+        state.posts = action.payload;
+      })
+      .addCase(fetchPosts.rejected, (state, action) => {
+        state.status = "failed";
+        state.error = action.error.message;
+      });
+  },
+});
+
+export const selectAllPosts = (state) => state.posts.posts;
 
 export default postSlice.reducer;
