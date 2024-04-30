ea284717838676aa42785986d8e64937197ec763
ahmetenesbahar <enesbahar2303@gmail.com>
2024-04-30 23:21:54 +0300

    ui: posts ui

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -1,27 +1,46 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchPosts } from "state/postSlice";
+import { useTheme } from "components/contexts/ThemeContext";
 
 const Posts = () => {
   const dispatch = useDispatch();
   const { posts, status, error } = useSelector((state) => state.posts);
   const token = useSelector((state) => state.auth.token);
+  const { theme } = useTheme();
 
   useEffect(() => {
     dispatch(fetchPosts(token));
   }, [dispatch, token]);
 
   return (
-    <div>
+    <div className="flex flex-col justify-center items-center max-w-[756px] w-full">
       {posts.map((post) => (
-        <div key={post._id}>
-          {console.log(post._id, post.title, post.picturePath)}
-          <p className="text-white">{post.title}</p>
-          <img
-            src={`http://localhost:3001/assets/${post?.picturePath}`}
-            alt=""
-            className="w-10 h-10"
-          />
+        <div
+          key={post._id}
+          className={`w-full mb-8 px-2 px-1  ${theme.hoverBackground} cursor-pointer rounded-lg`}
+        >
+          <div className={`flex items-center gap-1 `}>
+            <img
+              src={`http://localhost:3001/assets/${post.community?.picturePath}`}
+              alt=""
+              className="w-7 h-7 rounded-full"
+            />
+            <p className="text-sm font-medium opacity-90">
+              f/{post.community.communityName}{" "}
+              <span className="opacity-70 font-normal">• 1 day ago</span>
+            </p>
+          </div>
+          <p className="text-white font-medium text-xl">{post.title}</p>
+          {post.picturePath === "" ? null : (
+            <div className="w-full flex items-center justify-center ">
+              <img
+                src={`http://localhost:3001/assets/${post?.picturePath}`}
+                alt=""
+                className="max-w-[724px] max-h-[540px] rounded-lg"
+              />
+            </div>
+          )}
         </div>
       ))}
     </div>
