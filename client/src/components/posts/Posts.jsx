199fbd02a1b88b2caa154223f36c6d6318bf8e32
ahmetenesbahar <enesbahar2303@gmail.com>
2024-05-05 04:37:16 +0300

    ui: post background blur and border

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -37,14 +37,23 @@ const Posts = () => {
             <p className=" font-medium text-xl">{post.title}</p>
             <p className="mt-2">{post.content}</p>
             {post.picturePath === "" ? null : (
-              <div className="w-full relative flex items-center justify-center bg-contain rounded-xl mt-3">
+              <div
+                className={`w-full relative flex items-center justify-center bg-contain rounded-xl mt-3 border ${theme.borderGray}`}
+              >
                 <div
-                  className={`absolute max-w-[736px] max-h-[416px] w-full h-full ${theme.secondaryBackground} blur-md`}
+                  className={`absolute max-w-[722px] max-h-[430px] w-full h-full rounded-xl`}
+                  style={{
+                    backgroundImage: `url('http://localhost:3001/assets/${post?.picturePath}')`,
+                    backgroundSize: "350% 360%",
+                    backgroundPosition: "center",
+                    imageRendering: "smooth",
+                    filter: "blur(3px)",
+                  }}
                 />
                 <img
                   src={`http://localhost:3001/assets/${post?.picturePath}`}
                   alt=""
-                  className="z-40 max-w-[724px] max-h-[540px] rounded-lg object-contain select-none"
+                  className="z-10 max-w-[724px] max-h-[540px] h-full  object-contain select-none"
                 />
               </div>
             )}
@@ -52,7 +61,7 @@ const Posts = () => {
               <div
                 className={` relative w-20 h-9 flex items-center rounded-full space-between  ${theme.secondaryBackground}`}
               >
-                <div className={` w-9 h-9 flex items-center  `}>
+                <div className={`w-9 h-9 flex items-center  `}>
                   <PiArrowFatUp
                     className={` w-full h-full rounded-full px-2 py-1  cursor-pointer ${theme.upVote} ${theme.secondaryHoverBackground} ${theme.activeBackground}  `}
                   />
