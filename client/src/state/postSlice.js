c3e62337a378a47c985350fc1202a744fc1fec73
ahmetenesbahar <enesbahar2303@gmail.com>
2024-05-06 12:35:59 +0300

    refactor: this code has no usage anymore but i want it to stay uWu

--- client/src/state/postSlice.js
+++ client/src/state/postSlice.js
@@ -1,47 +1,46 @@
-import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+// import axios from "axios";
 
-export const fetchPosts = createAsyncThunk("post/getPosts", async (token) => {
-  try {
-    const response = await fetch("http://localhost:3001/posts/getPosts", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
-    if (!response.ok) {
-      throw new Error("Postlar alınamadı.");
-    }
-    const data = await response.json();
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
-});
+// export const fetchPosts = createAsyncThunk("post/getPosts", async (token) => {
+//   try {
+//     const response = await axios.get("http://localhost:3001/posts/getPosts", {
+//       headers: {
+//         "Content-Type": "application/json",
+//         Authorization: `Bearer ${token}`,
+//       },
+//     });
+//     return response.data;
+//   } catch (err) {
+//     console.error(err);
+//     throw err;
+//   }
+// });
 
-export const postSlice = createSlice({
-  name: "posts",
-  initialState: {
-    posts: [],
-    status: "idle",
-    error: null,
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(fetchPosts.pending, (state) => {
-        state.status = "loading";
-      })
-      .addCase(fetchPosts.fulfilled, (state, action) => {
-        state.status = "succeeded";
-        state.posts = action.payload;
-      })
-      .addCase(fetchPosts.rejected, (state, action) => {
-        state.status = "failed";
-        state.error = action.error.message;
-      });
-  },
-});
+// export const postSlice = createSlice({
+//   name: "posts",
+//   initialState: {
+//     posts: [],
+//     status: "idle",
+//     error: null,
+//   },
+//   extraReducers: (builder) => {
+//     builder
+//       .addCase(fetchPosts.pending, (state) => {
+//         state.status = "loading";
+//       })
+//       .addCase(fetchPosts.fulfilled, (state, action) => {
+//         state.status = "succeeded";
+//         state.posts = action.payload;
+//       })
+//       .addCase(fetchPosts.rejected, (state, action) => {
+//         state.status = "failed";
+//         state.error = action.error.message;
+//       });
+//   },
+// });
 
-export const selectAllPosts = (state) => state.posts.posts;
+// export const selectAllPosts = (state) => state.posts.posts;
 
-export default postSlice.reducer;
+// export default postSlice.reducer;
+
+//! Bu kodun kullanımına gerek kalmadı gibi gözüküyor, yine de burada bulunmasında fayda görüyorum. Bu kodun yerine usePosts.js dosyasındaki kodu kullanıyorum.
