bc533cfe71fe3cd14631b7b92fe5de617af204d6
ahmetenesbahar <enesbahar2303@gmail.com>
2024-05-12 16:12:55 +0300

    ui: login page x button ui

--- client/src/components/AuthPage.jsx
+++ client/src/components/AuthPage.jsx
@@ -20,7 +20,7 @@ const AuthPage = () => {
       rounded-xl flex items-center justify-center shadow-md relative `}
           >
             <RiCloseLine
-              className={`absolute rounded-full  w-8 h-8 right-2 top-2 ${theme.primary} text-white cursor-pointer`}
+              className={`absolute rounded-full  w-8 h-8 right-2 top-2 text-white cursor-pointer`}
               onClick={(e) => dispatch(setShowForm(false))}
             />
             <Form theme={theme} />
