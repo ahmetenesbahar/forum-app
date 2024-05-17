9cc23ce3de0446e5d2dd711950588388cd4d4a44
ahmetenesbahar <enesbahar2303@gmail.com>
2024-05-17 15:10:35 +0300

    refactor: Latest post vote display change

--- client/src/components/posts/LatestPosts.jsx
+++ client/src/components/posts/LatestPosts.jsx
@@ -42,7 +42,7 @@ const LatestPosts = () => {
             </div>
             <div>
               <p className={`text-xs mt-2 flex gap-1 ${theme.boxText}`}>
-                <PostVoteCount post={post} /> <span>·</span> 31 comments
+                <PostVoteCount post={post} /> votes <span>·</span> 31 comments
               </p>
             </div>
           </div>
