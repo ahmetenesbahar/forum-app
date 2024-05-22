7faf5039a508dba2f556c4611a28514c1dc8216b
ahmetenesbahar <enesbahar2303@gmail.com>
2024-05-22 17:40:28 +0300

    refactor: Update SearchResults component to display users, communities, and posts properly

--- client/src/components/SearchResults.jsx
+++ client/src/components/SearchResults.jsx
@@ -1,29 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import { useTheme } from "./contexts/ThemeContext";
 import UserDisplay from "./users/UserDisplay";
 import CommunityDisplay from "./communities/CommunityDisplay";
 import PostDisplay from "./posts/PostDisplay";
+import { RiArrowUpSLine } from "react-icons/ri";
 
 const SearchResults = ({ results }) => {
   const { theme } = useTheme();
+
   return (
     <div
-      className={`absolute w-full h-48 top-12 text-white  overflow-auto z-30 ${theme.secondaryBackground} rounded-xl overflow-auto scrollbar   ${theme.scrollbarTrack}  scrollbar-thin ${theme.scrollbarThumb} `}
+      className={`absolute w-full max-h-48 top-12 text-white  overflow-auto z-30 ${theme.secondaryBackground} rounded-xl overflow-auto scrollbar   ${theme.scrollbarTrack}  scrollbar-thin ${theme.scrollbarThumb} `}
     >
       <div className="flex flex-col">
-        <p className="text-lg font-medium px-4 py-1">Users</p>
-        <div className={`w-full h-px ${theme.grayBackground}`} />
         <UserDisplay results={results} />
-        <div>
-          <p className="text-lg font-medium px-4  py-1">Communities</p>
-          <div className={`w-full h-px ${theme.grayBackground}`} />
-          <CommunityDisplay results={results} />
-        </div>
-        <div>
-          <p className="text-lg font-medium px-4 py-1 ">Posts</p>
-          <div className={`w-full h-px ${theme.grayBackground}`} />
-          <PostDisplay results={results} />
-        </div>
+        <CommunityDisplay results={results} />
+        <PostDisplay results={results} />
       </div>
     </div>
   );
