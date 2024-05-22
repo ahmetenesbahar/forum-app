5fa28704b3e7cbf5c93bf023a0d4a22185c66cf2
ahmetenesbahar <enesbahar2303@gmail.com>
2024-05-22 18:10:02 +0300

    refactor: Add click outside functionality to hide search results

--- client/src/components/Searchbar.jsx
+++ client/src/components/Searchbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { RiSearchLine, RiCloseLine } from "react-icons/ri";
 import { useTheme } from "./contexts/ThemeContext";
 import useSearch from "hooks/useSearch";
@@ -6,11 +6,33 @@ import SearchResults from "./SearchResults";
 
 const Searchbar = () => {
   const { theme } = useTheme();
+  const searchRef = useRef(null);
+  const [showResults, setShowResults] = useState(true);
   const [parameter, setParameter] = useState("");
   const { data: result } = useSearch(parameter);
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (
+        showResults &&
+        searchRef.current &&
+        !searchRef.current.contains(event.target)
+      ) {
+        setTimeout(() => {
+          setShowResults(false);
+        }, 10);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showResults]);
+
   return (
-    <div className="flex flex-col relative">
+    <div className="flex flex-col relative" ref={searchRef}>
       <div className="relative flex items-center">
         <RiSearchLine className="absolute left-3 w-5 h-5" />
         <input
@@ -21,13 +43,14 @@ const Searchbar = () => {
           value={parameter}
           placeholder="Search on ForumD20"
           className={`${theme.secondaryBackground} ${theme.hoverBackground} rounded-full px-10 w-full h-10 focus:outline-none`}
+          onClick={() => setShowResults(true)}
         />
         <RiCloseLine
           className="absolute right-3 w-5 h-5 cursor-pointer"
           onClick={() => setParameter("")}
         />
       </div>
-      {parameter && <SearchResults results={result} />}
+      {parameter && showResults ? <SearchResults results={result} /> : ""}
     </div>
   );
 };
