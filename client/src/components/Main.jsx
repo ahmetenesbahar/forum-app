3b71dcb33faa88b0a5ea104dfbe5478f6b0301ea
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-08 18:12:29 +0300

    feat: Add ProfilePage component for displaying user profiles

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import ModalCenter from "./modals/ModalCenter";
 import Flow from "./Flow";
+import ProfilePage from "./users/ProfilePage";
 import PostDetail from "./posts/PostDetail";
 import { useTheme } from "./contexts/ThemeContext";
 
@@ -9,19 +10,32 @@ const Main = () => {
   const user = useSelector((state) => state.auth.user);
   const createTab = useSelector((state) => state.modal.showCreateModal);
   const postId = useSelector((state) => state.posts.postId);
+  const userId = useSelector((state) => state.users.userId);
   const { theme } = useTheme();
   const [selectedPost, setSelectedPost] = useState(null);
+  const [selectedProfile, setSelectedProfile] = useState(null);
 
   useEffect(() => {
     setSelectedPost(postId);
   }, [postId]);
+
+  useEffect(() => {
+    console.log(userId);
+    setSelectedProfile(userId);
+  }, [userId]);
 
   return (
     <div
       className={`w-full py-4 h-[calc(100vh-80px)] overflow-auto scrollbar ${theme.scrollbarThumb} ${theme.scrollbarTrack}  scrollbar-thin`}
     >
       {createTab & !(user === null) ? <ModalCenter type={"createTab"} /> : " "}
-      {selectedPost ? <PostDetail postId={postId} /> : <Flow />}
+      {selectedPost ? (
+        <PostDetail postId={postId} />
+      ) : selectedProfile ? (
+        <ProfilePage userId={userId} />
+      ) : (
+        <Flow />
+      )}
     </div>
   );
 };
