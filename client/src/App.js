47e1ffe38bdcea1e2ea5ea7179116017ab148e56
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-09 15:27:47 +0300

    refactor: Update App.js to include Sidebar, Navbar, AuthPage, ProfilePage, and PostDetail components

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,32 @@
 import React from "react";
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "components/HomePage";
+import Sidebar from "components/Sidebar";
+import Navbar from "components/Navbar";
+import AuthPage from "components/AuthPage";
+import Main from "components/Main";
 import { useTheme } from "components/contexts/ThemeContext";
+import ProfilePage from "components/users/ProfilePage";
+import PostDetail from "components/posts/PostDetail";
 
 const App = () => {
   const { theme } = useTheme();
 
   return (
-    <div className={` ${theme.background} ${theme.text}`}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <div className={` ${theme.background} ${theme.text}`}>
+        <Navbar />
+        <AuthPage />
+        <div className="flex">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/users/:userId" element={<ProfilePage />} />
+            <Route path="/posts/:postId" element={<PostDetail />} />
+            <Route path="*" element={<Navigate to="/" />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
   );
 };
 
