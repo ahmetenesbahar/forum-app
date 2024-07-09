ff321f46f4919680c7c2a84f917f4480bfe8c53a
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-09 15:38:13 +0300

    refactor: Remove unused code and update Main component

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -11,53 +11,13 @@ import { useTheme } from "./contexts/ThemeContext";
 const Main = () => {
   const user = useSelector((state) => state.auth.user);
   const createTab = useSelector((state) => state.modal.showCreateModal);
-  const postId = useSelector((state) => state.posts.postId);
-  const userId = useSelector((state) => state.users.userId);
   const { theme } = useTheme();
-  const dispatch = useDispatch();
-  const [navigationStack, setNavigationStack] = useState([]);
-
-  useEffect(() => {
-    if (postId !== null && !navigationStack.includes("post")) {
-      setNavigationStack([...navigationStack, "post"]);
-    }
-  }, [postId]);
-
-  useEffect(() => {
-    if (userId !== null && !navigationStack.includes("profile")) {
-      setNavigationStack([...navigationStack, "profile"]);
-    }
-  }, [userId]);
-
-  const handleGoBack = () => {
-    const updatedStack = [...navigationStack];
-    const lastPage = updatedStack.pop();
-    setNavigationStack(updatedStack);
-
-    if (lastPage === "profile") {
-      dispatch(setUserId(null));
-    } else if (lastPage === "post") {
-      dispatch(setPostId(null));
-    }
-  };
-
-  const renderContent = () => {
-    const lastPage = navigationStack[navigationStack.length - 1];
-    if (lastPage === "profile") {
-      return <ProfilePage userId={userId} handleGoBack={handleGoBack} />;
-    }
-    if (lastPage === "post") {
-      return <PostDetail postId={postId} handleGoBack={handleGoBack} />;
-    }
-    return <Flow />;
-  };
-
   return (
     <div
       className={`w-full py-4 h-[calc(100vh-80px)] overflow-auto scrollbar ${theme.scrollbarThumb} ${theme.scrollbarTrack} scrollbar-thin`}
     >
       {createTab && user ? <ModalCenter type={"createTab"} /> : " "}
-      {renderContent()}
+      <Flow />
     </div>
   );
 };
