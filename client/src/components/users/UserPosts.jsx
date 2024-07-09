5762a2e838e9b3f9bf6e68a0242c024fa67bf1b4
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-09 13:10:44 +0300

    refactor:UserPost display

--- client/src/components/users/UserPosts.jsx
+++ client/src/components/users/UserPosts.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
+import useUserPosts from "hooks/useUserPosts";
+import { useSelector } from "react-redux";
+import Post from "components/posts/Post";
 
-const UserPosts = () => {
-  return <div>UserPosts</div>;
+const UserPosts = ({ userId }) => {
+  const token = useSelector((state) => state.auth.token);
+  const { data: posts } = useUserPosts(token, userId);
+  useEffect(() => {
+    console.log(posts);
+  });
+  return (
+    <div className="flex flex-col items-center  w-full gap-y-2 ">
+      {posts
+        ?.slice()
+        .reverse()
+        .map((post) => (
+          <Post post={post} />
+        ))}
+    </div>
+  );
 };
 
 export default UserPosts;
