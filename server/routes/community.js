5df2c8fe2f15a4fcf0dfd2a5d59d17fcf09fd6fa
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-09 18:12:38 +0300

    refactor: Add getCommunity route to fetch a specific community by ID

--- server/routes/community.js
+++ server/routes/community.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { getCommunities } from "../controllers/community.js";
+import { getCommunities, getCommunity } from "../controllers/community.js";
 import { verifyToken } from "../middleware/auth.js";
 
 const router = express.Router();
 
 /*READ*/
 router.get("/", getCommunities);
+router.get("/:id", verifyToken, getCommunity);
+
 /*UPDATE*/
 
 export default router;
