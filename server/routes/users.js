ba8df30785d96bca466b577f951d18e7d366cd81
ahmetenesbahar <enesbahar2303@gmail.com>
2024-07-09 13:11:00 +0300

    refactor: Add getUserPosts, getUserUpvotes, and getUserDownvotes endpoints

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,6 +3,9 @@ import {
   getUser,
   getUserCommunities,
   addRemoveCommunities,
+  getUserPosts,
+  getUserUpVotes,
+  getUserDownVotes,
 } from "../controllers/user.js";
 import { verifyToken } from "../middleware/auth.js";
 
@@ -12,6 +15,9 @@ const router = express.Router();
 
 router.get("/:id", verifyToken, getUser);
 router.get("/:id/communities", verifyToken, getUserCommunities);
+router.get("/:id/getUserPosts", verifyToken, getUserPosts);
+router.get("/:id/getUserUpvotes", verifyToken, getUserUpVotes);
+router.get("/:id/getUserDownvotes", verifyToken, getUserDownVotes);
 
 /* UPDATE */
 
